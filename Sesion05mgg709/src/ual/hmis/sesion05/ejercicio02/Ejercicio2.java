c18e84e4e63fa0018fcf97097d0d1fd1fc2b117b
MATÍAS GARCÍA GIMENO <mgg709@inlumine.ual.es>
2022-04-08 14:44:58 +0200

    ejercicio 2 resuelto

--- Sesion05mgg709/src/ual/hmis/sesion05/ejercicio02/Ejercicio2.java
+++ Sesion05mgg709/src/ual/hmis/sesion05/ejercicio02/Ejercicio2.java
@@ -1,5 +1,28 @@
 package ual.hmis.sesion05.ejercicio02;
 
 public class Ejercicio2 {
+	
+	public boolean login (String username, String password){   
+		// comprobar que sean distintos de vacio  
+		if (username.equals(null))
+			return false;
+		if(password.equals(null))
+			return false;
+		// comprobar que la longitud sea < 30    
+		if(username.length()<30 && password.length()<30){
+		// llamar al método de la bbdd   
+		return compruebaLoginEnBD(username, password); 
+		}
+		else 
+			return false;
+	}    
+	public boolean compruebaLoginEnBD (String username, String password){   
+		// método mock (simulado)   
+		if (username.equals("user") && password.equals("pass"))    
+			return true;    
+		else     
+			return false;  
+		} 
+	}
 
-}
+
