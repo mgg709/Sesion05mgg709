b7394b3956fed1343fd8ad31131d6f0f639ed79d
MATÍAS GARCÍA GIMENO <mgg709@inlumine.ual.es>
2022-04-08 14:26:04 +0200

    subida ejercicio1

---
+++ Sesion05mgg709/test/ual/hmis/sesion05/ejercicio01/Ejercicio1Test.java
@@ -0,0 +1,27 @@
+package ual.hmis.sesion05.ejercicio01;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class Ejercicio1Test {
+
+	
+	@ParameterizedTest 
+	@CsvSource({
+		"4,1",
+		"9,1",
+		"35,7"
+	})
+	void testTransformar(int input, int expect) {
+		//Arrange
+		Ejercicio1 ej = new Ejercicio1();
+		//Act
+		int resultado = ej.transformar(input);
+		//Assert
+		assertEquals(expect, resultado);
+	}
+
+}
