87e13eb897b9cee0510cbb47266e072ca3d2f459
MATÍAS GARCÍA GIMENO <mgg709@inlumine.ual.es>
2022-04-26 18:46:41 +0200

    Renombrado del test del ejercicio 1

--- Sesion05mgg709/test/ual/hmis/sesion05/ejercicio01/Ejercicio01Test.java
+++ Sesion05mgg709/test/ual/hmis/sesion05/ejercicio01/Ejercicio01Test.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-class Ejercicio1Test {
+class Ejercicio01Test {
 
 	
 	@ParameterizedTest 
