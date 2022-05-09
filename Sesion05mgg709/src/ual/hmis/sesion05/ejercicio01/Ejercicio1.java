322a239fa04998f68f08f413195bb8a9af613ad7
MATÍAS GARCÍA GIMENO <mgg709@inlumine.ual.es>
2022-05-09 11:14:38 +0200

    actualizacion pom

--- Sesion05mgg709/src/ual/hmis/sesion05/ejercicio01/Ejercicio1.java
+++ Sesion05mgg709/src/ual/hmis/sesion05/ejercicio01/Ejercicio1.java
@@ -5,7 +5,7 @@ public class Ejercicio1 {
 	public int transformar (int x) {   
 		int resultado = 0;    
 		if (x % 2 == 0)  
-			// % Resto de una división entre enteros (mod)    
+			// % Resto de una division entre enteros (mod)    
 			resultado = transformar (x/2);   
 		else if (x % 3 == 0)    
 			resultado = transformar (x/3);   
